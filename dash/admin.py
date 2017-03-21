9832a1a1c49e0eb9f7637bd4371d2ee93a224fdd
root <root@salt-server.scloud.in>
2017-03-21 08:30:38 +0530

    Commit

--- dash/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Organization, UserProfile
-
-# Register your models here.
-admin.site.register(Organization)
-admin.site.register(UserProfile)
-
